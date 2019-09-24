70e9c8dfc2a5cf4e2721e9ae7015bf9e907d10e5
lilyj12 <lilyj12@gmail.com>
2019-09-24 14:30:43 -0700

    added model and some routes

--- apps/roulette/urls.py
+++ apps/roulette/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index), # to homepage
+    url(r'^optionsDisplay$', views.distance), # to page where they will choose distance
+    url(r'^roulett$', views.roulett), # will process their request and redirect to resultsDisplay
+    url(r'^resultsDisplay$', views.resultsDisplay), # to page that will display results
+    url(r'^viewInfo$', views.viewInfo), #to page that will display details about specific restaurant
+    url(r'^GoBack$', views.GoBack),# to go back to homepage and start over "delete session"
+    url(r'^loginReg$',views.loginReg),# to login and registration page
+    url(r'^newUser$', views.newUser),# to create the new user
+    url(r'^dashboard$', views.dashboard), # to display their personal dashboard
+    url(r'^edit$', views.edit) # to edit the user's favorites to add notes.
 ]
